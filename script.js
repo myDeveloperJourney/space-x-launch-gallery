fa884ea4f74aedf4f8396748054ef661ba7678e6
Daniel JS <daniel@mydeveloperjourney.com>
2022-08-11 14:26:47 -0500

    wip: sets up ipo pattern

---
+++ script.js
@@ -0,0 +1,18 @@
+// IPO -> Input Process Output
+
+// constants - variables that never change/get reassigned
+
+
+// state - variables that do change and affect functionality
+
+
+// cached element references
+
+
+// event listeners
+
+
+// functions
+
+
+
